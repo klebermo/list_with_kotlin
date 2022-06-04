cbfef5cab26dcf8a9765141181851c239dcc6534
Kleber Mota de Oliveira <klebermo@gmail.com>
2022-06-04 08:55:12 -0300

    bin deletion

--- app/bin/main/list/Node.kt
+++
@@ -1,9 +0,0 @@
-package list
-
-class Node<T>(var data: T? = null, var next: Node<T>? = null, var previous: Node<T>? = null) {
-    constructor(value: T) : this() {
-        this.data = value
-        this.next = null
-        this.previous = null
-    }
-}
